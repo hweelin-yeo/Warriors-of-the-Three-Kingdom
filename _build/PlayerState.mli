845a0d571ca8ddcc072b3f2aed9de5b31667062f
Yang Lu <yl882@cornell.edu>
2017-11-28 22:51:11 -0500

    bound states

---
+++ _build/PlayerState.mli
@@ -0,0 +1,34 @@
+(* PlayerState.ml
+
+ * [player_state] is a record type containing the following data:
+	- player_id
+	- player_score
+	- player_deck
+*)
+
+type playerID = string
+type cardID = string
+
+type player_state = {player_id: string;
+                     player_id_int: int;
+                     player_score: string;
+                     player_deck: string list
+                    }
+
+(* [generate_player_id_int i] generates the player_id_int, i, of a player *)
+val generate_player_id_int: int -> int
+
+(* [generate_player_id i] generates the player_id of a player *)
+val generate_player_id: int -> playerID
+
+(* [change_player_score s i] changes the score of a player by i *)
+val change_player_score: player_state -> int -> player_state
+
+(* [add_card s c] adds card c to the player_deck *)
+val add_card: player_state -> cardID -> player_state
+
+(* [remove_card s c] removes card c from the player_deck *)
+val remove_card: player_state -> cardID -> player_state
+
+(* [init_player_state i] initialises a player_state, with player_id_int i *)
+val init_player_state: int -> player_state
---
+++ _build/State.mli
@@ -0,0 +1,45 @@
+(* State.ml *)
+
+(* A state contains the following:
+   - description: description to be printed in REPL
+   - sec_description: secondary description to be printed in REPL
+   - current_player: the player who is currently having his/her turn
+   - recruit_pool: deck of centralised cards
+   - available_picks: 3 cards from recruit pool that current player can pick
+   - player_states: list of player_states, which will be elaborated later
+*)
+open Yojson
+open PlayerState
+open RecruitPile
+
+type state
+
+(* [change_available_picks s] returns a new state where
+   state's available_picks randomised   to be chosen by
+   player*)
+val change_available_picks: state -> state
+
+(* [change_description st s] returns a new state with
+   description s *)
+val change_description: state -> string -> state
+
+(* [change_sec_description st s] returns a new state with
+   secondary description s *)
+val change_sec_description: state -> string -> state
+
+(* [change_current_player st p] returns a new state with
+   the next player p *)
+val change_current_player: state -> playerID -> state
+
+(* [add_card s c] adds card c to the recruit_pool *)
+val add_card_recruit_pool: state -> cardID -> player_state
+
+(* [remove_card s c] removes card c from the recruit_pool *)
+val remove_card_recruit_pool: state -> cardID -> player_state
+
+(* [change_player_score s i] changes the score of a player by i *)
+val change_player_states: state -> state
+
+(* [init_state i j] initialises the state. it takes in int i,
+   the number of players, and a json file for the recruit pool*)
+val init_state: int -> Yojson.Basic.json -> state
