0a23cd9a34895560f0b6dae91202bf6bd1666c5a
Akira Shindo <as2568@cornell.edu>
2017-11-30 20:45:46 -0500

    some missing stuff to add

---
+++ Main.mli
@@ -0,0 +1,14 @@
+(* Main.mli *)
+(* you only need main.ml *)
+
+open RecruitPile
+open State
+open Cards
+open Ai
+
+(* [repl st] takes in the current state and returns a new state until the
+   game terminates in which it finally returns unit *)
+val repl: state -> unit
+
+(* [main] is the function that starts the game *)
+val main: unit -> unit
--- PlayerStateTest.ml
+++ PlayerStateTest.ml
@@ -10,8 +10,8 @@ let tests =
 "init_player_state_4" >:: (fun _ -> assert_equal [] (1 |> init_player_state).player_deck);
 "init_player_state_5" >:: (fun _ -> assert_equal 1 (1 |> init_player_state).player_resource);
 
-"change_player_score_1" >:: (fun _ -> assert_equal 5 (1 |> init_player_state |> change_player_score 5).player_score));
-"change_player_score_2" >:: (fun _ -> assert_equal 0 (1 |> init_player_state |> change_player_score -5).player_score));
+"change_player_score_1" >:: (fun _ -> assert_equal 5 (1 |> init_player_state |> change_player_score 5).player_score);
+"change_player_score_2" >:: (fun _ -> assert_equal 0 (1 |> init_player_state |> change_player_score (-5)).player_score);
 
 "add_card_1" >:: (fun _ -> assert_equal ["Card 1"] (1 |> init_player_state |> add_card "Card 1").player_deck);
 "add_card_2" >:: (fun _ -> assert_equal [] (1 |> init_player_state |> add_card "").player_deck);
@@ -24,7 +24,7 @@ let tests =
 "skip_turn_2" >:: (fun _ -> assert_equal 3 (1 |> init_player_state |> skip_turn |> skip_turn).player_resource);
 "skip_turn_3" >:: (fun _ -> assert_equal "Player 1" (1 |> init_player_state |> skip_turn).player_id);
 "skip_turn_4" >:: (fun _ -> assert_equal 1 (1 |> init_player_state |> skip_turn).player_id_int);
-"skip_turn_5" >:: (fun _ -> assert_equal 0 (1 |> init_player_state |> skip_turn).player_id_score);
+"skip_turn_5" >:: (fun _ -> assert_equal 0 (1 |> init_player_state |> skip_turn).player_score);
 "skip_turn_6" >:: (fun _ -> assert_equal [] (1 |> init_player_state |> skip_turn).player_deck);
 
 "change_player_resource_1" >:: (fun _ -> assert_equal 2 (1 |> init_player_state |> change_player_resource 1).player_resource);
