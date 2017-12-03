20d94debb8059d24145641bbb3f0b92382f95ad1
vagrant <vagrant@vm3110-2017fa>
2017-12-03 16:31:31 -0500

    work on the card set

--- State.ml
+++ State.ml
@@ -24,77 +24,50 @@ type card = {
   faction : string;
   power : int;
   flavor : string;
-  abilities : state -> int -> state;
+  card_text : string;
+  abilities : state -> int -> card list-> state;
   card_type : string;
 }
 
 
-(*
-let rec zhuge_liang_deck_helper (pd : player_deck) acc =
-  match pd with
-  | [] -> acc []
-  | h :: t -> if (h.faction = "Shu") then
-      zhuge_liang_deck_helper t (h :: h :: acc)
-    else
-      zhuge_liang_deck_helper t (h :: acc)
-
-let rec compute_deck_score (pd : player_deck) acc =
-  match pd with
-  | [] -> acc
-  | h :: t -> compute_deck_score t (h.power + acc)
-
-let rec new_players_list (psl : player_state list) (id : player_id_int) (npl : player_state) acc =
-  match psl with
-  | [] -> acc
-  | h :: t -> if h.player_id_int = id then new_player_list t id npl (npl :: acc)
-    else new_player_list t id npl (h :: acc)
-
-let zhuge_liang_funct (s : state) (p : player_state) (c_id : int) =
-  let new_deck = zhuge_liang_deck_helper p.player_deck in
-  let new_player_state = {
-    playerID = p.playerID;
-    player_id_int = p.player_id_int;
-    player_score = compute_player_score new_deck;
-    player_deck = new_deck;
-    player_resource = p.player_resource;
-    player_is_human = p.player_is_human;
-  }  in
-  let new_players_list s.player_states p.player_id_int new_player_state [] in
-  {
-    total_players = s.total_players;
-    card_drawn = s.card_drawn;
-    current_player = s.current_player;
-    recruit_pool = s.recruit_pool;
-    available_picks = s.available_picks;
-    player_states = new_players_list;
-  }*)
-
-
 (*Function for vanilla cards, simulates a blank abilities box thus
 the returned state is the same as the called state*)
-let vanilla (s : state) (cid : cardID) = s
+let vanilla (s : state) (cid : cardID) (cl : card list) = s
 
 (*id_to_card takes a card id int and returns the card object option associated with it.
   The inputs are the card id and the card list that represents the card set*)
 let rec id_to_card (id : cardID) (cl : card list) =
   match cl with
-  | [] -> None (*Should not happen*)
-  | h :: t -> if (h.card_id = id) then Some h
+  | [] -> failwith "shouldn't happen outside of debugging" (*Should not happen*)
+  | h :: t -> if (h.card_id = id) then h
     else
       id_to_card id t
 
 (*map_id_list maps a list of card ids to their respective cards*)
-let rec map_id_list (idl : cardID list) (cl : card list) acc =
+let rec map_id_list idl  (cl : card list) acc =
   match idl with
   | [] -> List.rev acc
   | h :: t -> map_id_list t cl ((id_to_card h cl) :: acc)
+
+(*map_card_list : maps a list of cards into a list of cardIDs*)
+let rec map_card_list (cl : card list) acc =
+  match cl with
+  | [] -> List.rev acc
+  | h :: t -> map_card_list t (h.card_id :: acc)
 
 (*find_player, given the list of int player_state tuples, and a int player id,
   return the corresponding player_state*)
 let rec find_player (psl : (int * player_state) list) (id : int) =
   match psl with
   | [] -> failwith "Player not found"
-  | h :: t -> if (fst h = id) then h else find_player t id
+  | h :: t -> if (fst h = id) then snd h else find_player t id
+
+(*find_opponents finds the states of all opponents*)
+let rec find_opponents (psl : (int * player_state) list) (id : int) acc =
+  match psl with
+  | [] -> List.rev acc
+  | h :: t -> if (fst h = id) then find_opponents t id acc else
+      find_opponents t id (snd h :: acc)
 
 
 (*compute_deck_score takes in a card_list that represents the deck and returns
@@ -104,10 +77,92 @@ let rec compute_deck_score (cl : card list) acc =
   | [] -> acc
   | h :: t -> compute_deck_score t (h.power + acc)
 
+let rec make_new_states (pl : player_state) (ps : (int * player_state) list) acc =
+  match ps with
+  | [] -> List.rev acc
+  | h :: t -> if (pl.player_id_int = fst h)
+    then make_new_states pl t ((pl.player_id_int, pl) :: acc) else
+      make_new_states pl t (h :: acc)
+
+
+(*Zhuge liang helper*)
+let rec zhuge_liang_helper (pd : card list) acc =
+  match pd with
+  | [] -> List.rev acc
+  | h :: t -> if (h.faction = "Shu") then zhuge_liang_helper t (h :: h :: acc)
+    else
+      zhuge_liang_helper t (h :: acc)
+
 (*zhuge_liang_funct*)
-(*let zhuge_liang_funct (s : state) (cid : int) =
+let zhuge_liang_funct (s : state) (cid : int) (cl : card list) =
   let currentPlayerInt = s.current_player in
-  let currentPlayer = find_player currentPlayerInt s.player_states in *)
+  let currentPlayer = find_player s.player_states currentPlayerInt in
+  let current_deck_ids = currentPlayer.player_deck in
+  let current_deck = map_id_list current_deck_ids cl [] in
+  let new_deck = zhuge_liang_helper current_deck [] in
+  let new_deck_score = compute_deck_score new_deck 0 in
+  let new_deck_ids = map_card_list new_deck [] in
+  let newPlayer1 = {currentPlayer with player_deck = new_deck_ids} in
+  let newPlayer = {newPlayer1 with player_score = new_deck_score} in
+  (*Generate new state object*)
+  let new_player_states = make_new_states newPlayer s.player_states [] in
+  {s with player_states = new_player_states}
+
+(*Shu Recruiter functions, pd = player deck, nc = num copies*)
+let rec shu_recruiter_helper (pd : int list) (nc : int) acc =
+  match nc with
+  | 0 -> acc
+  | x -> shu_recruiter_helper pd (nc - 1) (1 :: acc)
+
+let shu_recruiter_funct (s : state) (cid : int) (cl : card list) =
+  let currentPlayerInt = s.current_player in
+  let currentPlayer = find_player s.player_states currentPlayerInt in
+  let current_deck_ids = currentPlayer.player_deck in
+  let num_units_created = Random.int 4 in
+  let new_deck_ids = shu_recruiter_helper current_deck_ids num_units_created current_deck_ids in
+  let new_deck = map_id_list new_deck_ids cl [] in
+  let new_player_score = compute_deck_score new_deck 0 in
+  let newPlayer1 = {currentPlayer with player_deck = new_deck_ids} in
+  let newPlayer = {newPlayer1 with player_score = new_player_score} in
+  let new_player_states = make_new_states newPlayer s.player_states [] in
+  {s with player_states = new_player_states}
+
+(*Sima Yi functions*)
+let sima_yi_helper (ids : int list) =
+  (*h1 = 7, id of Sima Yi*)
+  match ids with
+  | [] -> []
+  | h1 :: h2 :: h3 :: h4 :: t -> 7 :: t
+  | h :: t -> [7] (*All other cases with less than 3 units get eliminated*)
+
+let sima_yi_funct (s : state) (cid : int) (cl : card list) =
+  let currentPlayerInt = s.current_player in
+  let currentPlayer = find_player s.player_states currentPlayerInt in
+  let current_deck_ids = currentPlayer.player_deck in
+  let new_deck_ids = sima_yi_helper current_deck_ids in
+  let new_deck = map_id_list new_deck_ids cl [] in
+  let new_player_score = compute_deck_score new_deck 0 in
+  let newPlayer1 = {currentPlayer with player_deck = new_deck_ids} in
+  let newPlayer = {newPlayer1 with player_score = new_player_score} in
+  let new_player_states = make_new_states newPlayer s.player_states [] in
+  {s with player_states = new_player_states}
+
+(*Xiahou Dun Functions*)
+
+let xiahou_dun_funct (s : state) (cid : int) (cl : card list) =
+  let currentPlayerInt = s.current_player in
+  let opponentList = find_opponents s currentPlayerInt in
+  s
+
+
+
+
+(*Tiago Chan Function, Snapcaster Mage Senpai!!!! ^.^*)
+(*let tiago_chan_funct (s : state) (cid : int) (cl : card list) =
+  let currentPlayerInt = s.current_player in
+  let currentPlayer = find_player s.player_states currentPlayerInt in
+  let current_deck_ids = currentPlayer.player_deck in
+  let current_deck = map_id_list current_deck_ids cl [] in *)
 
 
 
@@ -120,23 +175,106 @@ let cardList = [
     faction = "Shu";
     power = 1;
     flavor = "Liu Bei's soldiers were recruited from the farmhands of \n China";
+    card_text = "This card has no abilities";
     abilities = vanilla;
       card_type = "Soldier"
   };
 
   {
     card_name = "Zhuge Liang, Sleeping Dragon";
-    card_id = 1;
+    card_id = 2;
     cost = 4;
     faction = "Shu";
-    power = 1;
+    power = 2;
     flavor = "The greatest strategist of the Three Kingdoms was the Sleeping \n
 Dragon, Zhuge Liang";
-    (*Dummy testing value here*)
-    abilities = vanilla;
-(*fun (s : state) (p : player_state) (c_id : int) ->
-                  zhuge_liang_funct s p c_id;*)
+    card_text = "When you draft Zhuge Liang, Sleeping Dragon, for each \n Shu card in your deck, copy that card and add it to your deck";
+    abilities = zhuge_liang_funct;
       card_type = "Soldier"
+  };
+
+  {
+    card_name = "Shu Recruiter";
+    card_id = 3;
+    cost = 3;
+    faction = "Shu";
+    power = 1;
+    flavor = "The Shu army came on like a swarm of ants, \n each man who was slain was replaced with two brothers";
+    card_text = "When you draft Shu Recruiter, add a random number 0-4 of \n Shu Footsoldier tokens into your deck";
+    abilities = shu_recruiter_funct;
+    card_type = "Soldier"
+  };
+
+  {
+    card_name = "Sima Yi, Traitor to Wei";
+    card_id = 7;
+    cost = 4;
+    faction = "Wei";
+    power = 10;
+    flavor = "Not the Lius, not the Suns, nor the Caos would conquer China, \n for the ursurper Sima clan would conquer all";
+    card_text = "When you draft Sima Yi Traitor to Wei discard the top 3 cards of your deck";
+    abilities = sima_yi_funct;
+    card_type = "Soldier"
+  };
+
+  {
+    card_name = "Wei Elite Infantry";
+    card_id = 8;
+    cost = 3;
+    faction = "Wei";
+    power = 3;
+    flavor = "Cao Cao's soldiers were all hand picked, veterans of countless \n conflicts";
+    card_text = "No Abilities";
+    abilities = vanilla;
+    card_type = "Soldier";
+  };
+
+  {
+    card_name = "Xiahou Dun, The One-Eyed";
+    card_id = 9;
+    cost = 4;
+    faction = "Wei";
+    power = 2;
+    flavor = "He used to have two eyes. Until he ate one";
+    card_text = "When you draft Xiahou Dun, discard the top card from each \n opponent's deck";
+    abilities = vanilla;
+    card_type = "Soldier";
+  };
+
+  {
+    card_name = "Wu Boatsman";
+    card_id = 14;
+    cost = 2;
+    faction = "Wu";
+    power = 2;
+    flavor = "Soldiers of Wu fought not for glory, but for their families and for \n their homeland";
+    card_text = "No Abilities";
+    abilities = vanilla;
+    card_type = "Solider";
+  };
+
+  {
+    card_name = "Lu Bu, Mad Demon";
+    card_id = 20;
+    cost = 6;
+    faction = "Other";
+    power = 8;
+    flavor = "Of the warriors of the Three Kingdoms, there were none stronger than \n Lu Bu";
+    card_text = "No Abiitities";
+    abilities = vanilla;
+    card_type = "Soldier";
+  };
+
+  {
+    card_name = "Tiago Chan";
+    card_id = 21;
+    cost = 5;
+    faction = "Other";
+    power = 2;
+    flavor = "Snapcaster Mage, but better";
+    card_text = "When you draft Tiago Chan, cast each spell in your deck again";
+    abilities = vanilla; (*Will definitely be changed later*)
+    card_type = "Soldier";
   }
 ]
 
