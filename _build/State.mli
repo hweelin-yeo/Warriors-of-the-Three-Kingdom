ebe8a1f6f0501872e0c469810d48aea800547355
Akira Shindo <as2568@cornell.edu>
2017-12-03 19:52:28 -0500

    aitest

---
+++ _build/State.mli
@@ -0,0 +1,53 @@
+(* State.ml *)
+
+(* A state contains the following:
+   - description: description to be printed in REPL
+   - sec_description: secondary description to be printed in REPL
+   - current_player: the player who is currently having his/her turn
+   - recruit_pool: deck of centralised cards
+   - available_picks: 3 cards from recruit pool that current player can pick
+   - player_states: list of player_states, which will be elaborated later
+*)
+open Yojson
+open PlayerState
+
+type state
+type playerID = string
+type cardID = int
+
+
+
+(* [change_next_player st ] returns a new state for the next player *)
+val change_next_player: state -> state
+
+(* [change_current_player st p] returns a new state with
+   the next player p
+ * Use case: skip function
+ * requires: p <= state.total_players *)
+val change_to_player: state -> int -> state
+
+
+(*--------------- KIV -------------------------*)
+(* [change_description st s] returns a new state with
+   description s *)
+(* val change_description: state -> string -> state *)
+(* [change_sec_description st s] returns a new state with
+   secondary description s *)
+(* val change_sec_description: state -> string list-> state *)
+(*--------------- KIV -------------------------*)
+
+
+
+(* [pick_card st c] returns a new state with
+   card drawn as some c *)
+val draw_card: state -> cardID -> state
+
+(* [add_card s c] adds card c to the recruit_pool *)
+val add_card_recruit_pool: state -> cardID -> state
+
+(* [remove_card s c] removes card c from the recruit_pool *)
+val remove_card_recruit_pool: state -> cardID -> state
+
+(* [init_state i j] initialises the state. it takes in int i,
+   the number of players, and a json file for the recruit pool*)
+val init_state: int -> int -> state
---
+++ _build/statetest.ml
@@ -0,0 +1,39 @@
+open OUnit2
+open State
+open RecruitPile
+
+
+let init_state_1 = init_state 4 1 (* file *)
+(* Tests for value of each field for file recruitpile.json*)
+let init_state_test =
+  [
+    "total_players" >:: (fun _ -> assert_equal 4 (init_state_1.total_players));
+    "card_drawn" >:: (fun _ -> assert_equal None (init_state_1.card_drawn));
+    "current_player" >:: (fun _ -> assert_equal 1 (init_state_1.current_player));
+    (* "recruit_pool" >:: (fun _ -> assert_equal 1 (init_state_1.r)); *)
+    "available_picks" >:: (fun _ -> assert_equal 3 (init_state_1.available_picks |> List.length));
+    "player_states" >:: (fun _ -> assert_equal 3 (init_state_1.player_state |> List.length));
+]
+
+let pick_card_state_1 = init_state 4 1 |> draw_card
+let pick_card_state =
+  [
+    "total_players" >:: (fun _ -> assert_equal 4 (init_state_1.total_players));
+    "card_drawn" >:: (fun _ -> assert_equal None (init_state_1.card_drawn));
+    "current_player" >:: (fun _ -> assert_equal 1 (init_state_1.current_player));
+    (* "recruit_pool" >:: (fun _ -> assert_equal 1 (init_state_1.r)); *)
+    "available_picks" >:: (fun _ -> assert_equal 3 (init_state_1.available_picks |> List.length));
+    "player_states" >:: (fun _ -> assert_equal 3 (init_state_1.player_state |> List.length));
+  ]
+(* tests for do' function for all possible commands *)
+let do_test =
+  [
+  ]
+
+let suite =
+  "Adventure test suite"
+  >::: List.flatten [
+    init_state_test
+  ]
+
+let _ = run_test_tt_main suite
